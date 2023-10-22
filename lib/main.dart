35bdf86556e0c8bd353a989eabedb936adebb1a8
hieungch <hieuntgch18057@fpt.edu.vn>
2023-10-22 20:50:24 +0700

    step-17 update notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttynotes/constants/routes.dart';
 import 'package:fluttynotes/services/auth/auth_service.dart';
 import 'package:fluttynotes/views/login_view.dart';
-import 'package:fluttynotes/views/notes/new_note_view.dart';
+import 'package:fluttynotes/views/notes/create_update_note_view.dart';
 import 'package:fluttynotes/views/notes/notes_view.dart';
 import 'package:fluttynotes/views/register_view.dart';
 import 'package:fluttynotes/views/verify_email_view.dart';
@@ -26,7 +26,7 @@ void main() {
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
-      newNoteRoute: (context) => const NewNoteView(),
+      createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
     },
   ));
 }
