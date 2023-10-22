eefe7d2abb5a5d904bdd4f3dbd628129bf7a7137
hieungch <hieuntgch18057@fpt.edu.vn>
2023-10-22 20:17:13 +0700

    step-16

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:fluttynotes/constants/routes.dart';
 import 'package:fluttynotes/services/auth/auth_exceptions.dart';
 import 'package:fluttynotes/services/auth/auth_service.dart';
-import 'package:fluttynotes/utilities/show_error_dialog.dart';
+
+import '../utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
